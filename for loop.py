daf5ecd0896872c786d096c63116a58d68b38141
ydevil2009 <85282146+ydevil2009@users.noreply.github.com>
2022-06-17 16:25:52 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,22 @@
+from tkinter import * 
+
+root=Tk()
+root.title("Ascii")
+root.geometry("400x400")
+root.configure(background= 'snow')
+
+enter_word = Entry(root)
+enter_word.place(relx=0.5,rely=0.4,anchor=CENTER)
+
+label = Label(root, text = "Name in Ascii :", bg="light yellow", fg="black")
+
+def asciiConverter():
+    input_word = enter_word.get()
+    for letter in input_word :
+        label["text"] += str(ord(letter)) + "  "
+
+btn=Button(root,text="Show Name in Ascii",command=asciiConverter, bg='gold', fg = "black")
+btn.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+label.place(relx=0.5,rely=0.6,anchor=CENTER)
+root.mainloop()
